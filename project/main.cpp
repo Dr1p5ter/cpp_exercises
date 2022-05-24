0241920612e228c0ef3d3147c9d144fc3046ff91
Matthew McLaren <mmclaren@purdue.edu>
2022-05-24 18:59:55 -0400

    Main() => Seperates String By Spaces

--- project/main.cpp
+++ project/main.cpp
@@ -1,15 +1,37 @@
 #define STOP (0)
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-  for (int i = 0; i < 10; i += 1)
+  /* grab current word */
+  string resp;
+  cout << "Give me some words: ";
+  getline(cin, resp);
+  resp.push_back('\n');
+
+  /* print individual word */
+  vector<string> words;
+  words.push_back("");
+  int wordIndex = 0;
+  for (auto c : resp)
   {
-    cout << "i = " << i << "\n";
+    if ((c == ' ') || (c == '\n'))
+    {
+      cout << "word " << wordIndex << " is " << words.at(wordIndex) << endl;
+      words.push_back("");
+      wordIndex += 1;
+    }
+    else
+    {
+      words.at(wordIndex) = words.at(wordIndex) + c;
+    }
   }
-  return STOP;
+  cout << "Word count is " << wordIndex << endl;
+  return 0;
 }
 
